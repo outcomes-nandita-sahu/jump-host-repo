4ab822a1871602447cb3091fd2eb1e36605ec502
cah-harshith-kk <103484781+cah-harshith-kk@users.noreply.github.com>
2024-02-29 09:34:49 -0500

    Add files via upload

---
+++ PolicyPatientRepoDataIdentifier.sql
@@ -0,0 +1,42 @@
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4088
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4086
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4084
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4085
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=3308
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=3923
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4083
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=2731
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4082
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4087
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4486
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=3863
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=2273
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=2265
+delete from outcomes.dbo.PolicyPatientRepoDataIdentifier where policyID=4277
+
+
+
+
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4541),4541,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4542),4542,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4543),4543,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4544),4544,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4545),4545,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4546),4546,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4547),4547,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4548),4548,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4549),4549,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4550),4550,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4551),4551,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4552),4552,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4557),4557,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4558),4558,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4553),4553,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4554),4554,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4555),4555,1,'PHARMACY',getDate());
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4556),4556,1,'PHARMACY',getDate());
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4563),4563,1,'PHARMACY',getDate()); 
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4564),4564,1,'PHARMACY',getDate());
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4565),4565,1,'PHARMACY',getDate());
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4566),4566,1,'PHARMACY',getDate());
+insert into outcomes.dbo.PolicyPatientRepoDataIdentifier values ((select policyName from outcomes.dbo.Policy where policyID=4485),4485,1,'PHARMACY',getDate());
